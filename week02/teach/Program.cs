c2e02f07c8e2869911f79d32bdf2f305096a0e3f
andersonhavah <havahanderson@gmail.com>
2025-07-20 21:35:27 +0000

    Added code to run ComplexStack

--- week02/teach/Program.cs
+++ week02/teach/Program.cs
@@ -1,3 +1,8 @@
 ﻿Console.WriteLine("\n======================\nCustomer Service\n======================");
-CustomerService.Run();
-// CustomerServiceSolution.Run();
+// CustomerService.Run();
+CustomerServiceSolution.Run();
+
+
+Console.WriteLine("\n======================\nComplex Stack\n======================");
+// ComplexStack.Main();
+ComplexStackSolution.Main();
